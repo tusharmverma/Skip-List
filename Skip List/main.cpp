0c6a88a90b848f44a5813302c2922d1ca85a886e
Tushar Verma <tusharverma@Tushars-MacBook-Pro.local>
2017-09-20 19:34:30 -0500

    Initial Commit

---
+++ Skip List/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Skip List
+//
+//  Created by Tushar  Verma on 9/20/17.
+//  Copyright © 2017 Tushar Verma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
